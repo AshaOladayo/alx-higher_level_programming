c5656179afd5c487d7b987465afea1c83d7c1869
AshaOladayo <ashayeoladayo@gmail.com>
2024-01-12 14:43:11 -0800

    done

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from add_0 import add
+    import add_0 as plus
     a = 1
     b = 2
-    c = add(a,b)
+    c = plus(a, b)
     print(f"{a} + {b} = {c}")
