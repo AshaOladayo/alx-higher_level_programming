3c3871cc85a264ad3002b94126633fe236106db1
AshaOladayo <ashayeoladayo@gmail.com>
2024-01-12 15:15:09 -0800

    done

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import add_0
+    import add_0 as plus
     a = 1
     b = 2
-    c = add(a, b)
-    print("{} + {} = {}".format(a, b, c))
+    
+    print(f"{a} + {b} = {plus.add(a, b)}")
